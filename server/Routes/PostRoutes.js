c151f06c060c79241d4043091c03e6a36efcbe36
yashnarkhedkar <yashnarkhedkar@gmail.com>
2023-01-21 14:09:36 +0530

    change

--- server/Routes/PostRoutes.js
+++ server/Routes/PostRoutes.js
@@ -1,45 +1,43 @@
-import Express from "express";
-import * as dotenv from 'dotenv'
-import { v2 as cloudinary } from 'cloudinary'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
 import Post from '../models/post.js';
 
-dotenv.config()
-const router = Express.Router();
+dotenv.config();
+
+const router = express.Router();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-// GET ALL POSTS
 router.route('/').get(async (req, res) => {
-    try {
-        const post = await Post.find({});
-        res.status(200).json({ success: true, data: post });
-    } catch (error) {
-        res.status(500).json({ success: false, message: error });
-    }
-})
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
 
-
-
-// CREATE A POST
 router.route('/').post(async (req, res) => {
-    try {
-        const { name, prompt, photo } = req.body;
-        const photoUrl = await cloudinary.uploader.upload(photo);
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
-        const newPost = await Post.create({
-            name,
-            prompt,
-            photo: photoUrl.url,
-        })
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
 
-        res.status(201).json({ success: true, data: newPost });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error })
-    }
-})
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+  }
+});
 
 export default router;
